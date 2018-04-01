587f8368891ecb6c2bf67fe0002137e705ca2a31
zydarChen <zydarChen@outlook.com>
2018-04-01 14:34:45 +0800

    Site updated: 2018-04-01 14:34:38

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -97,3 +97,12 @@ $(document).ready(function () {
       }) : NexT.utils.displaySidebar();
   }
 });
+
+// 设置折叠快
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    $("div.fold").css("display","none");
+});
